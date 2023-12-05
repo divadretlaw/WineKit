5cf7076c191ef33ba7a7fe36321ced5f7aafb1d3
David Walter <hello@davidwalter.at>
2023-12-05 08:25:45 +0100

    Add more documentation

--- Sources/PortableExecutable/Machine.swift
+++ Sources/PortableExecutable/Machine.swift
@@ -13,34 +13,65 @@ import Foundation
 /// [PE Format - Machine Types](https://learn.microsoft.com/en-us/windows/win32/debug/pe-format#machine-types)
 /// on *Microsoft Learn*.
 public enum Machine: UInt16, Hashable, Equatable, CustomStringConvertible, Sendable {
+    /// The content of this field is assumed to be applicable to any machine type
     case unknown = 0x0
+    /// Alpha AXP, 32-bit address space
     case alpha = 0x184
+    /// Alpha 64, 64-bit address space or AXP 64
+    ///
+    /// Alpha 64 and AXP 64 share the same value.
+    /// Matsushita AM33
     case alpha64orAXP64 = 0x284 // Alpha 64 and AXP 64 share the same value
     case am33 = 0x1d3
+    /// x64
     case amd64 = 0x8664
+    /// ARM little endian
     case arm = 0x1c0
+    /// ARM64 little endian
     case arm64 = 0xaa64
+    /// ARM Thumb-2 little endian
     case armNt = 0x1c4
+    /// EFI byte code
     case ebc = 0xebc
+    /// Intel 386 or later processors and compatible processors
     case i386 = 0x14c
+    /// Intel Itanium processor family
     case ia64 = 0x200
+    /// LoongArch 32-bit processor family
     case loongArch32 = 0x6232
+    /// LoongArch 64-bit processor family
     case loongArch64 = 0x6264
+    /// Mitsubishi M32R little endian
     case m32r = 0x9041
+    /// MIPS16
     case mips16 = 0x266
+    /// MIPS with FPU
     case mipsFpu = 0x366
+    /// MIPS16 with FPU
     case mipsFpu16 = 0x466
+    /// Power PC little endian
     case powerPc = 0x1f0
+    /// Power PC with floating point support
     case powerPcFp = 0x1f1
+    /// MIPS little endian
     case r4000 = 0x166
+    /// RISC-V 32-bit address space
     case riscV32 = 0x5032
+    /// RISC-V 64-bit address space
     case riscV64 = 0x5064
+    /// RISC-V 128-bit address space
     case riscV128 = 0x5128
+    /// Hitachi SH3
     case sh3 = 0x1a2
+    /// Hitachi SH3 DSP
     case sh3dsp = 0x1a3
+    /// Hitachi SH4
     case sh4 = 0x1a6
+    /// Hitachi SH5
     case sh5 = 0x1a8
+    /// Thumb
     case thumb = 0x1c2
+    /// MIPS little-endian WCE v2
     case wceMipsV2 = 0x169
     
     // MARK: - CustomStringConvertible
